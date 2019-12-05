c858c5869d716a46c086ec37415dbb1bd4a78600
klimkomx <klimkomx@gmail.com>
2019-12-05 13:34:00 +0300

    Added compressor.cpp and decompressor.cpp

---
+++ compressor.cpp
@@ -0,0 +1,227 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ios>
+#include <set>
+#include <iostream>
+#include <cstring>
+#include <windows.h>
+
+struct treeNode {
+    treeNode *left, *right;
+    int count;
+    bool terminate;
+    char symbol;
+    treeNode(){
+        left = right = nullptr;
+        terminate = false;
+        count = 0;
+    }
+};
+
+struct compare {
+    bool operator() (const treeNode * a, const treeNode* b)  const{
+        if (a -> count < b -> count)
+            return 1;
+        else if (a -> symbol < b -> symbol)
+            return 1;
+        return 0;
+    }
+};
+//bool compare(treeNode * a, treeNode*& b)  {
+//    return a -> count < b -> count;
+//}
+
+void getCodes(treeNode* root, std::vector<bool> code[256], std::vector<bool>& path) {
+    if (root -> terminate) {
+        int num = ((unsigned int)root -> symbol);
+//        std::cout << num << "\n";
+        if (num < 0) num = 127 - num;
+        code[num] = path;
+    }
+    else {
+        path.push_back(false);
+        getCodes(root -> left, code, path);
+        path.pop_back();
+        path.push_back(true);
+        getCodes(root -> right, code, path);
+        path.pop_back();
+    }
+    return;
+}
+
+void compress(std::string inputFile, std::string outputFile, const std::string name) {
+    std::cout << "opened" << name << "\n";
+    unsigned char temporary;
+    long long cnt[256];
+    int counte;
+    treeNode * node, *least1, *least2;
+    std::multiset<treeNode*, compare> haffList;
+    std::vector<bool> code[256];
+    bool used[256];
+
+    //initialisation
+    counte  = 0;
+    std::fill(cnt, cnt + 255, 0);
+    std::fill(used, used + 255, 0);
+
+    //files
+    std::ifstream input(inputFile, std::ios::binary);
+
+    std::cout << inputFile;
+    for (int i = 0; i < 1e9; i+= 2) {
+        i--;
+    }
+    if (!input.is_open()) {
+        std::cout << "Something went wrong...";
+        return;
+    }
+    std::cout << "passed3";
+    for (int i = 0; i < 1e9; i+= 2) {
+        i--;
+    }
+    std::ofstream output(outputFile, std::ios::binary);
+    if (!output.is_open()) {
+        std::cout << "Something went wrong...";
+        return;
+    }
+    std::cout << "passed2";
+    for (int i = 0; i < 1e9; i+= 2) {
+        i--;
+    }
+	//counting
+	while (input.read((char *)&temporary, sizeof(char))) {
+        used[(unsigned int) temporary] = true;
+        cnt[(unsigned int) temporary]++;
+	}
+	std::cout << "passed3";
+    for (int i = 0; i < 1e9; i+= 2) {
+        i--;
+    }
+	input.close();
+	std::cout << "opened" << name << "222\n";
+	//building list to sort
+	for (int i = 0; i < 256; i++) {
+	    if (used[i]) {
+	        ++counte;
+	        node = new treeNode();
+	        node -> symbol = (char)i;
+	        node -> terminate = true;
+	        node -> count = cnt[i];
+	        haffList.insert(node);
+	    }
+	}
+	std::cout << "opened" << name << "111\n";
+	//bulding Haffman tree
+	for (int i = 0; i < counte - 1; ++i) {
+	    least1 = *haffList.begin();
+	    haffList.erase(haffList.begin());
+        least2 = *haffList.begin();
+        haffList.erase(haffList.begin());
+
+        node = new treeNode();
+        node -> left = least1;
+        node -> right = least2;
+        node -> count = least1 -> count + least2 -> count;
+        node -> symbol = (char)127;
+        haffList.insert(node);
+	}
+	std::vector<bool> pathh = {};
+	getCodes(*haffList.begin(), code, pathh);
+    std::ifstream input2(inputFile, std::ios::binary);
+
+
+    //making output file
+    output << (unsigned char)name.size();
+    for (int i = 0; i < name.size(); i++)
+        output << name[i];
+    unsigned int blockscount =  0u;
+    long long rawOutputLength = 0ll;
+    for (int i = 0; i < 256; i++)
+        if (used[i])
+            blockscount++;
+    output << (unsigned char)(blockscount>>8) << (unsigned char)blockscount;
+    for (int i = 0; i < 256; i++) {
+        if (!used[i])
+            continue;
+        rawOutputLength += ((long long)cnt[i])*((long long)code[i].size());
+        output << (unsigned char)i << (unsigned char)code[i].size();
+        unsigned char outchar = 0u;
+        int counter = 0;
+        for (int j = 0; j < code[i].size(); j++) {
+            outchar <<= 1;
+            if (code[i][j]) outchar |= 1u;
+            counter++;
+            if (counter == 8) {
+                output << (unsigned char)outchar;
+                counter = 0;
+            }
+        }
+        if (counter != 0) {
+            output << (unsigned char)(outchar << (8 - counter));
+        }
+    }
+    output << (unsigned char)(rawOutputLength >> 56) << (unsigned char)(rawOutputLength >> 48) << (unsigned char)(rawOutputLength >> 40) << (unsigned char)(rawOutputLength >> 32) << (unsigned char)(rawOutputLength >> 24) << (unsigned char)(rawOutputLength >> 16) << (unsigned char)(rawOutputLength >> 8) << (unsigned char)(rawOutputLength >> 0);
+    unsigned char temporaryChar = 0u, inputc;
+    unsigned char outchar = 0u;
+    int counter = 0;
+    while (input2.read((char *)&inputc, sizeof(char))) {
+        for (int j = 0; j < code[inputc].size(); j++) {
+            outchar <<= 1;
+
+            if (code[inputc][j]) outchar |= 1u;
+            counter++;
+            if (counter == 8) {
+                output << (unsigned char)outchar;
+                counter = 0;
+            }
+        }
+    }
+    if (counter != 0) {
+        output << (unsigned char)(outchar << (8 - counter));
+    }
+	input2.close();
+	output.close();
+    return;
+}
+int main2() {
+    setlocale(LC_ALL, "russian");
+    char a[56] = "D:\\Downloads\\t_down\\meme";
+
+    compress(a, "D:\\Downloads\\meme.xxx", "kek");
+    return 0;
+}
+int main(int argc, const char *argv[])
+{
+    std::string argv1 = std::string(argv[1]);
+    if (argc != 2)
+        return 0;
+    std::string path = "", name = "";
+    int nlength = argv1.size(), it;
+    std::cout << argv1 << "\n";
+    for (it = nlength - 1; it >= 0; --it) {
+        if (argv[1][it] == '\\' || argv[1][it] == '/')
+            break;
+    }
+    ++it;
+    for (int i = 0; i  < it; i++) {
+        path.push_back(argv1[i]);
+    }
+    while (it < nlength && argv1[it] != '.') {
+        path.push_back(argv1[it]);
+        name.push_back(argv1[it]);
+        ++it;
+    }
+    path.push_back('.');
+    path.push_back('x');
+    path.push_back('x');
+    path.push_back('x');
+    while (it < nlength) {
+        name.push_back(argv1[it]);
+        ++it;
+    }
+
+    compress(argv1 , path, name);
+    return 0;
+}
---
+++ decompressor.cpp
@@ -0,0 +1,173 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ios>
+#include <algorithm>
+#include <set>
+#include <string.h>
+#include <iostream>
+#include <map>
+struct treeNode {
+    treeNode *left, *right;
+    int count;
+    bool terminate;
+    char symbol;
+    treeNode(){
+        left = right = nullptr;
+        terminate = false;
+        count = 0;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    if (argc != 2)
+        return 0;
+    int nlength = strlen(argv[1]);
+    if (nlength < 5 || argv[1][nlength-1] != 'x' || argv[1][nlength-2] != 'x' || argv[1][nlength-3] != 'x' || argv[1][nlength-4] != '.') {
+        return 0;
+    }
+    std::ifstream input(argv[1], std::ios::binary);
+
+    unsigned char inputuchar;
+    char inputchar;
+
+    if (!input.read((char *)&inputuchar, sizeof(char))) {
+        return 0;
+    }
+
+    std::string outputname = "";
+    int it;
+    char argv1[nlength];
+    for (int i = 0; i < nlength; i++)
+        argv1[i] = argv[1][i];
+    for (it = nlength - 1; it >= 0; --it) {
+        if (argv[1][it] == '\\' || argv[1][it] == '/')
+            break;
+    }
+    ++it;
+    for (int i = 0; i  < it; i++) {
+        outputname.push_back(argv[1][i]);
+    }
+
+    unsigned int outputnamesize = (unsigned int)inputuchar;
+    if (outputnamesize < 0) outputnamesize = 127 - outputnamesize;
+    for (int i = 0; i < outputnamesize; i++) {
+        if (!input.read((char *)&inputchar, sizeof(char))) {
+            return 0;
+        }
+        outputname.push_back(inputchar);
+    }
+    std::cout << "this is name : " << outputname << "\n";
+
+    std::ofstream output(outputname, std::ios::binary);
+    if (!output)
+        return 0;
+
+
+
+    treeNode* root = new treeNode();
+    root -> left = root;
+    root -> right = root;
+    treeNode* temproot = root;
+    unsigned int numofsymb = 0, temp = 0, blocksize = 0u;
+    int blocknum;
+    char symbolourselves;
+
+
+
+    if (!input.read((char *)&inputuchar, sizeof(char))) {
+        std::cout << "Something went wrong...";
+        return 0;
+    }
+    temp = (unsigned int)inputuchar;
+    numofsymb<<=8;
+    numofsymb|=temp;
+    if (!input.read((char *)&inputuchar, sizeof(char))) {
+        std::cout << "Something went wrong...";
+        return 0;
+    }
+    temp = (unsigned int)inputuchar;
+    numofsymb<<=8;
+    numofsymb|=temp;
+    std::cout << numofsymb << "numofsym\n";
+    for (unsigned int i = 0u; i < numofsymb; ++i) {
+        temproot = root;
+        if (!input.read((char *)&symbolourselves, sizeof(char))) {
+            std::cout << "Something went wrong...";
+            return 0;
+        }
+        if (!input.read((char *)&inputuchar, sizeof(char))) {
+            std::cout << "Something went wrong...";
+            return 0;
+        }
+        temp = (unsigned int) inputuchar;
+        std::cout << temp << " temp N" << i << "numofsym\n";
+        std::cout << "'" << symbolourselves << "' - symbol\n";
+        while (temp > 0u) {
+            if (!input.read((char *)&inputuchar, sizeof(char))) {
+                std::cout << "Something went wrong...";
+                return 0;
+            }
+            int iter = 7;
+            while (temp > 0u && iter >= 0) {
+                std::cout << ((inputuchar>>iter)&1 ? 1 : 0);
+                if ((inputuchar>>iter)&1) {
+                    if (temproot -> right == root) {
+                        temproot -> right = new treeNode();
+                        temproot -> right -> right = temproot -> right -> left = root;
+                    }
+                    temproot = temproot -> right;
+                } else {
+                    if (temproot -> left == root) {
+                        temproot -> left = new treeNode();
+                        temproot -> left -> right = temproot -> left -> left = root;
+                    }
+                    temproot = temproot -> left;
+                }
+                --temp;
+                --iter;
+            }
+            std::cout << " k\n";
+        }
+        temproot -> terminate = true;
+        temproot -> symbol = symbolourselves;
+    }
+
+    unsigned long long rawdatasize = 0ull, rawtemp = 0ull;
+    for (int i = 0; i < 8; i++) {
+        if (!input.read((char *)&inputuchar, sizeof(char))) {
+            std::cout << "Something went wrong...";
+            return 0;
+        }
+        rawtemp = (unsigned long long)inputuchar;
+        rawdatasize<<=8;
+        rawdatasize|=rawtemp;
+    }
+    std::cout << rawdatasize << " rawdsize\n";
+
+    while (rawdatasize > 0ull) {
+        if (!input.read((char *)&inputuchar, sizeof(char))) {
+            std::cout << "Something went wrong...";
+            return 0;
+        }
+        int iter = 7;
+        while (rawdatasize > 0ull && iter >= 0) {
+            if ((inputuchar>>iter)&1) {
+                root = root -> right;
+            } else {
+
+                root = root -> left;
+            }
+            if (root->terminate) {
+                output << root -> symbol;
+                root = root -> left;
+            }
+
+            --rawdatasize;
+            --iter;
+        }
+    }
+    input.close();
+    output.close();
+    return 0;
+}
